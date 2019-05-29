e42380995f4107d09819239abe5c827c24e50935
SarahSchmit <sarah.schmit@student.ap.be>
2019-05-29 12:27:42 +0200

    upload project

---
+++ menu.js
@@ -0,0 +1,16 @@
+/*  menu knop */
+
+let menuKnop = document.querySelector(".menu");
+let navMenu = document.querySelector("nav");
+let mainGedeelte = document.querySelector("main");
+
+function verbergMenu() {
+	navMenu.classList.add("onzichtbaar");
+}
+
+mainGedeelte.addEventListener("click",verbergMenu);
+function toonMenu() {
+	navMenu.classList.remove("onzichtbaar");
+}
+
+menuKnop.addEventListener("click",toonMenu);
